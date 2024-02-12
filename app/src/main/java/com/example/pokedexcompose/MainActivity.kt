b8ec10d605eccc92c215d93878efce48d7a76ce3
VASS\daniel.hidalgop <daniel.hidalgop@vasscompany.com>
2024-02-12 11:50:55 +0100

    Empty App

---
+++ app/src/main/java/com/example/pokedexcompose/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.pokedexcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pokedexcompose.ui.theme.PokedexComposeTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+
